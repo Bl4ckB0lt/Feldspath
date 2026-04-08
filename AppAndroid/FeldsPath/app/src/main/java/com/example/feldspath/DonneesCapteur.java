fd0f1aa27444136ec0f0e8f6de6c70ec21e5e325
Maxime <maxi.labat@gmail.com>
2026-04-08 11:11:10 +0200

    Modif de la classe métier donnée

--- AppAndroid/FeldsPath/app/src/main/java/com/example/feldspath/DonneesCapteur.java
+++ AppAndroid/FeldsPath/app/src/main/java/com/example/feldspath/DonneesCapteur.java
@@ -3,5 +3,35 @@ package com.example.feldspath;
 public class DonneesCapteur {
     float gaz;
     float humidite;
-    float temperature;
+    double temperature;
+
+    public DonneesCapteur(float gaz, float humidite, double temperature) {
+        this.gaz = gaz;
+        this.humidite = humidite;
+        this.temperature = temperature;
+    }
+
+    public float getGaz() {
+        return gaz;
+    }
+
+    public void setGaz(float gaz) {
+        this.gaz = gaz;
+    }
+
+    public float getHumidite() {
+        return humidite;
+    }
+
+    public void setHumidite(float humidite) {
+        this.humidite = humidite;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
 }
